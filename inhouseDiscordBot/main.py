b3ff8d0838201fdcf5ca3daf6f5088b7cbbe4630
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-06-19 17:09:32 -0600

    Added commands to move users to channels

--- inhouseDiscordBot/main.py
+++ inhouseDiscordBot/main.py
@@ -14,15 +14,31 @@ def main():
 
     @client.event
     async def on_ready():
-        print("Bot is ready")
-        print("----------------")
+        print("Bot has come online")
+
 
 
     @client.command()
-    async def moveChat(ctx):
-        await ctx.send("Feature currently under development.")
+    async def moveToTeams(ctx):
+        mainChannel = discord.utils.get(ctx.guild.channels, name="Main")
+        teamChannel = discord.utils.get(ctx.guild.channels, name="team-a")
+        members = mainChannel.members
+        for member in members:
+            await member.move_to(teamChannel)
+            await ctx.send("Moved " + member.name + " from " + mainChannel.name + " to " + teamChannel.name)
 
-
+    @client.command()
+    async def moveToMain(ctx):
+        mainChannel = discord.utils.get(ctx.guild.channels, name="Main")
+        teamAChannel = discord.utils.get(ctx.guild.channels, name="team-a")
+        teamBChannel = discord.utils.get(ctx.guild.channels, name="team-b")
+        aMembers = teamAChannel.members
+        bMembers = teamBChannel.members
+        for member in aMembers:
+            await member.move_to(mainChannel)
+        for member in bMembers:
+            await member.move_to(mainChannel)
+        await ctx.send("Moved all users from " + teamAChannel.name + " and " + teamBChannel.name + " to " + mainChannel.name)
     
     client.run(config.token)
 
