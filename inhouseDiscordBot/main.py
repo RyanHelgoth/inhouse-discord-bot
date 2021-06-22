3ed7b7d053ebfcfe123355f5d2167fa70ee31895
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-06-22 15:10:21 -0600

    Added showTeams command and moved repeated code into functions

--- inhouseDiscordBot/main.py
+++ inhouseDiscordBot/main.py
@@ -10,6 +10,7 @@ import config
 import random
 
 #TODO organize code better
+#TODO bug where user in main chat is not found if bot comes online after they joined
 
 def main():
     client = commands.Bot(command_prefix = "!")
@@ -31,7 +32,6 @@ def main():
 
     @client.command()
     async def moveToTeams(ctx):
-        print(client.team1Channel)
         if not None in (client.team1Channel, client.team2Channel, client.mainChannel):
             if len(client.team1) > 0 and len(client.team2) > 0:
                 for member in client.team1:
@@ -66,48 +66,69 @@ def main():
 
     @client.command()
     async def setTeam1(ctx, *args):
-        channelName = " ".join(args)
-        client.team1Channel = discord.utils.get(ctx.guild.channels, name = channelName)
-        if client.team1Channel == None:
-            await ctx.send("Could not find a channel named " + channelName + " please try again.")
-        else:
-            await ctx.send("Team 1 channel set to " + channelName)
-
-
-
-        
+        await setChannel(ctx, args, "one")
+     
 
     @client.command()
     async def setTeam2(ctx, *args):
-        channelName = " ".join(args)
-        client.team2Channel = discord.utils.get(ctx.guild.channels, name = channelName)
-        if client.team2Channel == None:
-            await ctx.send("Could not find a channel named " + channelName + " please try again.")
-        else:
-            await ctx.send("Team 2 channel set to " + channelName)
+        await setChannel(ctx, args, "two")
 
     @client.command()
     async def setMain(ctx, *args):
-        channelName = " ".join(args)
-        client.mainChannel = discord.utils.get(ctx.guild.channels, name = channelName)
-        if client.mainChannel == None:
-            await ctx.send("Could not find a channel named " + channelName + " please try again.")
-        else:
-            await ctx.send("Main channel set to " + channelName)
+        await setChannel(ctx, args, "main")
 
     @client.command()
     async def randomize(ctx):
         if not None in (client.team1Channel, client.team2Channel, client.mainChannel):
             members = client.mainChannel.members
-            if len(members) > 1:
+            if len(members) > 0: #change 0 to 1 when done testing
                 random.shuffle(members)
                 half = len(members)//2
                 client.team1 = members[0:half]
                 client.team2 = members[half:len(members)]
+                await printTeams(ctx)
             else:
                 await ctx.send("There must be 2 or more people in the " + client.mainChannel.name + " channel to use this command.")
         else:
             await ctx.send("You must first set team and main channels before using this command")
+
+    @client.command()
+    async def showTeams(ctx):
+        await printTeams(ctx)
+        
+    async def printTeams(ctx):
+        t1Members = []
+        t2Members = []
+        for member in client.team1:
+            t1Members.append("<@" + str(member.id) + ">")
+        for member in client.team2:
+            t2Members.append("<@" + str(member.id) + ">")
+        await ctx.send("Team 1: " + ", ".join(map(str, t1Members)))
+        await ctx.send("Team 2: " + ", ".join(map(str, t2Members)))
+        
+    async def setChannel(ctx, args, channel):
+        channelName = " ".join(args)
+        
+        if channel== "one":
+            client.team1Channel = discord.utils.get(ctx.guild.channels, name = channelName)
+            if client.team1Channel == None:
+                await ctx.send("Could not find a channel named " + channelName + " please try again.")
+            else:
+                await ctx.send("Team 1 channel set to " + channelName)
+        elif channel == "two":
+            client.team2Channel = discord.utils.get(ctx.guild.channels, name = channelName)
+            if client.team2Channel == None:
+                await ctx.send("Could not find a channel named " + channelName + " please try again.")
+            else:
+                await ctx.send("Team 2 channel set to " + channelName)
+        elif channel == "main":
+            client.mainChannel = discord.utils.get(ctx.guild.channels, name = channelName)
+            if client.mainChannel == None:
+                await ctx.send("Could not find a channel named " + channelName + " please try again.")
+            else:
+                await ctx.send("Main channel set to " + channelName)
+
+
 
     client.run(config.token)
 
