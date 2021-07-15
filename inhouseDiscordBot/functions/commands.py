54472ffa05ddb0a66cbc648784cd8e3a26bc4f98
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2021-07-14 21:56:33 -0600

    Improved wording of bot messages

--- inhouseDiscordBot/functions/commands.py
+++ inhouseDiscordBot/functions/commands.py
@@ -47,6 +47,7 @@ async def help(ctx):
 This function displays the members in team 1 and 2.
 '''
 async def printTeams(ctx, db):
+    userID = ctx.message.author.id
     teams = {"team1" : [], "team2" : []} #Keys are db vars and values are for holding user ids.
     docRef = ch.getDocRef(ctx, db)
     doc = docRef.get()
@@ -61,16 +62,16 @@ async def printTeams(ctx, db):
                     try:
                         memberID = int(memberID) 
                     except ValueError:
-                        await ctx.send("Error: {} is corrupted, please make {} again.".format(teamName)) 
+                        await ctx.send("Error: <@{0}>'s {1} is corrupted, please make your {1} again.".format(userID, teamName)) 
                         return
                     
                     member = ctx.guild.get_member(memberID)
                     if member is None:
-                        await ctx.send("Error: could not find one or more users, please make {} again.".format(teamName))
+                        await ctx.send("Error: could not find one or more users in <@{0}>'s {1}, please make your {1} again.".format(userID, teamName))
                         return
                     else:
                         teams[team].append("<@" + str(memberID) + ">")
-            await ctx.send(":video_game: {}: {}".format(teamName, ", ".join(map(str, teams[team]))))
+            await ctx.send("<@{0}>'s :video_game: {1}: {2}".format(userID, teamName, ", ".join(map(str, teams[team]))))
     else:
         await ctx.send("Could not find any saved teams, please create teams.")
 
@@ -83,6 +84,7 @@ async def randomizeMain(ctx, db):
     Randomization is done by shuffling the members in the main channel
     and then spliting the list of members in half to form two teams.
     '''
+    userID = ctx.message.author.id
     docRef = ch.getDocRef(ctx, db)
     doc = docRef.get()
 
@@ -92,55 +94,56 @@ async def randomizeMain(ctx, db):
             try:
                 channelID = int(data["voiceMain"]) 
             except ValueError:
-                await ctx.send("Error: main channel preference was corrupted, please set it again.") 
+                await ctx.send("Error: <@{}>'s main channel preference was corrupted, please set it again.".format(userID)) 
                 return
             
             mainChannel = discord.utils.get(ctx.guild.channels, id = channelID) 
             if mainChannel is None:
-                await ctx.send("Error: couldn't find mainChannel, please try setting it again.") 
+                await ctx.send("Error: couldn't find <@{}>'s main channel, please try setting it again.".format(userID)) 
             else:
                 members = mainChannel.members
                 memberIDS = [str(member.id) for member in members]
                 if len(memberIDS) > 0: #TODO change 0 to 1 when done testing
                     random.shuffle(memberIDS)
-                    half = len(memberIDS)//2
+                    half = len(memberIDS) // 2
                     team1 = memberIDS[0:half]
                     team2 = memberIDS[half:len(memberIDS)]
                     data = {"team1" : team1, "team2" : team2}
                     docRef.set(data, merge = True)
                     await printTeams(ctx, db)
                 else:
-                    await ctx.send("There must be 2 or more people in the " + "<#" 
-                    + str(channelID) + ">" + " channel to use this command.") 
+                    await ctx.send("There must be 2 or more people in the <#{}> channel to use this command.".format(channelID)) 
         else:
-            await ctx.send("You must first set the main channel before using this command") 
+            await ctx.send("You must first set your main channel preference before using this command.") 
     else:
-        await ctx.send("You must first set the main channel before using this command")
+        await ctx.send("You must first set your main channel preference before using this command.")
 
 '''
 This function lets the user pick a preference for their main
 channel and then saves it in the database. 
 '''
 async def setChannel(ctx, db, args, chat):
+    userID = ctx.message.author.id
     channelName = " ".join(args)
     docRef = ch.getDocRef(ctx, db)
     chats = {"Team 1" : "voice1", "Team 2" : "voice2", "Main" : "voiceMain"} #Values are the variable names in the db.
     channel = discord.utils.get(ctx.guild.channels, name = channelName)
 
     if channel is None:
-        await ctx.send("Could not find a channel named \"" + channelName + "\" please try again.")
+        await ctx.send("Could not find a channel named \"{}\" please try again.".format(channelName))
     elif str(channel.type) != "voice":
-        await ctx.send("<#" + str(channel.id) + ">" + " is not a voice channel, you can only set voice channels.")
+        await ctx.send("<#{}> is not a voice channel, you can only set voice channels.".format(channel.id))
     else:
         data = {chats[chat] : str(channel.id)}
         docRef.set(data, merge = True)
-        await ctx.send("{} channel set to ".format(chat) + "<#" + str(channel.id) + ">")
+        await ctx.send("<@{}>'s {} channel set to <#{}>".format(userID, chat, channel.id))
 
 '''
 This function lets the user make a team and then then saves the team 
 in the database.
 '''
 async def makeTeam(ctx, db, args, teamName):
+    userID = ctx.message.author.id
     docRef = ch.getDocRef(ctx, db)
     doc = docRef.get()
     members = [] #NOTE this list now contains user id strings and not member objects.
@@ -152,13 +155,13 @@ async def makeTeam(ctx, db, args, teamName):
         try:
             memberID = int(arg[3:-1]) #Strips away characters to get id from <@id>
         except ValueError:
-            await ctx.send("Error: you entered an invalid user id. Please tag users you want to add, for example: @userName")
+            await ctx.send("Error: you entered an invalid user. Please tag users you want to add by typing \"@\" and selecting users.")
             return
         
         member = ctx.guild.get_member(memberID)
         if member is None:
             await ctx.send("Error: could not find one or more selected users. Please tag users you want to add, " + 
-            "for example: @userName. Please also seperate each tagged user with 1 space. ")
+            "by typing \"@\" and selecting users. Please also seperate each tagged user with 1 space.")
             return
 
         if not member in members:
@@ -180,7 +183,7 @@ async def makeTeam(ctx, db, args, teamName):
                 opposingTeam = list(set(opposingTeam) - set(members))
                 data = {opposingTeamRef : opposingTeam}
                 docRef.set(data, merge = True)
-                await ctx.send("Note: some members have been moved from {} to {}.".format(opposingTeamName.lower(), teamName.lower())) 
+                await ctx.send("Note: some members have been moved from <@{0}>'s {1} to <@{0}>'s {2}.".format(userID, opposingTeamName.lower(), teamName.lower())) 
     
     data = {teamRef : members}
     docRef.set(data, merge = True)
